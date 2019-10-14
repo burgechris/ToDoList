c6bdcf87eb1f95bd967a2d81f577ac60ffefdb32
Student <example@example.com>
2019-10-14 14:19:44 -0700

    add database functionality.

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -4,54 +4,36 @@ using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
+    public class ItemsController : Controller
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+        [HttpGet("/categories/{categoryId}/items/new")]
+        public ActionResult New(int categoryId)
+        {
+            Category category = Category.Find(categoryId);
+            return View(category);
+        }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Show(int categoryId, int itemId)
+        {
+            Item item = Item.Find(itemId);
+            Category category = Category.Find(categoryId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("item", item);
+            model.Add("category", category);
+            return View(model);
+        }
 
-     [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
+        [HttpPost("/categories/{categoryId}/items/delete")]
+        public ActionResult DeleteAll(int categoryId)
+        {
+            Category foundCategory = Category.Find(categoryId);
+            List<Item> categoryItems = foundCategory.Items;
+            categoryItems.Clear();
+            return View(categoryId);
+        }
     }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
-    }
-  }
 }
 
 
